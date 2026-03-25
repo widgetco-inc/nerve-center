7268b428736d0a9e5d6c6a59c0b6352d9aee9f6a
widgetco-inc <jg@widgetco.com>
2026-03-24 21:45:05 -0500

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import BerkshireHathawayDashboard from './OwnersDashboard.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+      <BerkshireHathawayDashboard />
+        </React.StrictMode>
+        )
